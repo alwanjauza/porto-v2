bb4d793208fda2870d05fb8b573f8f702c4c365f
Alwan Jauza <alwanjrosyad.12345@gmail.com>
2025-11-21 13:24:06 +0700

    feat: add robots.txt and sitemap.ts for SEO optimization

---
+++ app/sitemap.ts
@@ -0,0 +1,20 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://www.alwan-projects.me";
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+  ];
+}
